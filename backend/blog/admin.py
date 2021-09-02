e0db3e76e98a416f797b3b8dfb019b75636dea31
landersoft <landersoft@gmail.com>
2021-09-01 21:00:01 -0400

    personalizar admin

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
+from blog.models import Perfil, Post , Tag
 
 # Register your models here.
+
+@admin.register(Perfil)
+class PerfilAdmin(admin.ModelAdmin):
+    model = Perfil
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    model = Tag
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+
+    list_display = (
+        "id",
+        "titulo",
+        "subtitulo",
+        "slug",
+        "fecha_publicacion",
+        "publicado",
+    )
+
+    list_filter = (
+        "publicado",
+        "fecha_publicacion",
+    )
+
+    list_editable = (
+        "titulo",
+        "subtitulo",
+        "slug",
+        "fecha_publicacion",
+        "publicado",
+    )
+
+    search_fields = (
+        "titulo",
+        "subtitulo",
+        "slug",
+        "cuerpo"
+    )
+
+    prepopulated_fields = {
+        "slug": (
+            "titulo",
+            "subtitulo",
+        )
+    }
+
+    date_hierarchy = "fecha_publicacion"
+    save_on_top = True
